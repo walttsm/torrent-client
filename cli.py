a3fc8dbe23ca65a1fcb9ddff33f41d938965df43
Walter Marinho <waltersmarinho@edu.unifil.br>
2022-04-17 16:39:55 -0300

    Classe CLI e config de execução

---
+++ cli.py
@@ -0,0 +1,27 @@
+import sys
+import time
+
+import config
+
+def connecting():
+    if not config.VERBOSE:
+        print('\rConectando...', end='\b')
+        
+def connected(total):
+    if not config.VERBOSE:
+        print('\rConectado!     ')
+        loading(0, total)
+         
+def loading(iteration, total):
+    if not config.VERBOSE:
+        filled = int(100 * iteration // total)
+        progress_bar = '\033[94m•\033[0m' * filled + '◦' * (100 - filled)
+        print('\r{} {}/{}'.format(progress_bar, iteration, total), end='\b')
+        if iteration == total:
+            print()
+            print('\033[92m✔\033[0m Complete in {:.3f}s!'.format(time.time() - config.START_TIME))
+            print()
+            
+def printf(message, prefix=''):
+    if config.VERBOSE:
+        print(prefix.ljust(20), message)
---
+++ config.py
@@ -0,0 +1,7 @@
+VERBOSE = True
+BLOCK_SIZE = 2 ** 14
+PIECE_SIZE = 0
+START_TIME = None
+
+tracker = None
+manager = None
